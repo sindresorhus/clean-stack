885a918c0b15ede43f677dd1ab03f6289970fca8
Thomas Sileghem <th.sileghem@gmail.com>
2016-10-12 19:44:04 +0100

    feat: correctly remove babel-polyfill paths

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 'use strict';
-module.exports = stack => stack.replace(/\\/g, '/').replace(/\s+at.*\((?:(?:internal\/)?\w+\.js:\d+:\d+|native)\)|\s+at.*(?:node|internal\/\w+)\.js:\d+:\d+/g, '');
+
+const extractPathRegex = /\s+at.*(?:\(|\s)(.*)\)?/;
+const pathRegex = /^(?:(?:(?:node|(?:internal\/|.*node_modules\/babel-polyfill\/.*)?\w+)\.js:\d+:\d+)|native)/;
+
+module.exports = stack => {
+	return stack.replace(/\\/g, '/')
+		.split('\n')
+		.filter(x => {
+			const pathMatches = x.match(extractPathRegex);
+			if (pathMatches === null || !pathMatches[1]) {
+				return true;
+			}
+
+			return !pathRegex.test(pathMatches[1]);
+		})
+		.filter(x => x.trim() !== '')
+		.join('\n');
+};
