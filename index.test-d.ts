00073faa1c4993ff2b2b8e9afcc738f1fe4c8cef
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-07 09:35:40 +0200

    Add TypeScript definition

---
+++ index.test-d.ts
@@ -0,0 +1,9 @@
+import {expectType} from 'tsd';
+import cleanStack = require('.');
+
+const error = new Error('Missing unicorn');
+
+if (error.stack) {
+	expectType<string>(cleanStack(error.stack));
+	expectType<string>(cleanStack(error.stack, {pretty: true}));
+}
