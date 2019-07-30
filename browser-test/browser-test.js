7875f789d3a62e9b266029754681073605c9d56c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-30 14:21:01 +0200

    Fix linting in browser-test.js and exclude it from further linting

--- browser-test/browser-test.js
+++ browser-test/browser-test.js
@@ -1,9 +1,9 @@
-import { ClientFunction, Selector } from 'testcafe';
+import {ClientFunction, Selector} from 'testcafe';
 
-fixture `Getting Started`
-    .page `./index.html`;
+fixture`Getting Started`
+	.page`./index.html`;
 
 test('default', async t => {
-    const cleanStackTest = ClientFunction(() => window.module.exports('    Error: Not cleanable line\n    at process.emit (events.js:172:7)'));
-    await t.click(Selector('#awaiter')).expect(cleanStackTest()).eql('    Error: Not cleanable line');
+	const cleanStackTest = ClientFunction(() => window.module.exports('    Error: Not cleanable line\n    at process.emit (events.js:172:7)'));
+	await t.click(Selector('#awaiter')).expect(cleanStackTest()).eql('    Error: Not cleanable line');
 });
