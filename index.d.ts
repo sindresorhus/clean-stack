2908185439e2970412897bdfc0b8a0bc1ae00514
Sindre Sorhus <sindresorhus@gmail.com>
2023-03-21 16:16:24 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -30,7 +30,7 @@ export type Options = {
 	//     at Object.<anonymous> (/Users/sindresorhus/dev/clean-stack/unicorn.js:2:15)
 	//     at Object.<anonymous> (/Users/sindresorhus/dev/clean-stack/omit-me.js:1:16)
 
-	const pathFilter = path => /omit-me/.test(path);
+	const pathFilter = path => !/omit-me/.test(path);
 
 	console.log(cleanStack(error.stack, {pathFilter}));
 	// Error: Missing unicorn
